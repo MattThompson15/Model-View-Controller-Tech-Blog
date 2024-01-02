e57fc7daf177bf4b3688693ef355dd52a688f1cb
Matt Thompson <matthewthompson118@gmail.com>
2024-01-02 13:56:44 -0400

    modified authcontroller.js

---
+++ controllers/authController.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const { User } = require('../models');
+const router = express.Router();
+
+router.post('/register', async (req, res) => {
+    try {
+        
+    }
+})
