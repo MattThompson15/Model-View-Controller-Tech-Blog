b6915f1181b3abf31984b6c89a08246b5e21475b
Matt Thompson <matthewthompson118@gmail.com>
2024-01-02 16:07:41 -0400

    modified postcontroller.js

--- controllers/postController.js
+++ controllers/postController.js
@@ -44,3 +44,60 @@ router.get('/create', (req, res) => {
     res.render('create-post');
 });
 
+router.post('/create', async (req, res) => {
+    try {
+        const { title, content } = req.body;
+        const userId = req.session.user.id;
+
+        const post = await Post.create({ title, content, userId });
+        res.redirect(`/posts/post/${post.id}`);
+    } catch (error) {
+        console.error('Error creating a new post:', error);
+        res.status(500).json({ error: 'Internal server error'});
+    }
+});
+
+router.get('/edit/:id', async (req, res) => {
+    try {
+        const postId = req.params.id;
+        const post = await Post.findByPk(postId);
+
+        if (!post) {
+            res.status(404).json({ error: 'Post not found' });
+            return;
+        }
+
+        res.render('edit-post', { post });
+    }   catch(error) {
+        console.error('Error fetching post details for editing', error);
+        res.status(500).json({ error: 'Internal server error'});
+    };
+})
+
+router.post('/edit/:id', async (req, res) => {
+    try {
+        const postId = req.params.id;
+        const { title, content } = req.body;
+
+        await Post.update({ title, content }, { where: { id: postId} });
+        res.redirect(`/posts/post/${postId}`);
+    }   catch (error) {
+        console.error('Error updating the post:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+router.post('/delete/:id', async (req, res) => {
+    try {
+        const postId = req.params.id;
+
+        await Post.destroy({ where: { id: postId } });
+        res.redirect('/posts');
+    }   catch (error) {
+        console.error('Error deleting the post', error);
+        res.status(500).json({ error: 'Internal server error'});
+    }
+});
+
+module.exports = router;
+
