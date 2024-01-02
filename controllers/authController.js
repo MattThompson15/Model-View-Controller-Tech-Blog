098dc6b810cd0e371a9a5890112c9293d9b99ded
Matt Thompson <matthewthompson118@gmail.com>
2024-01-02 14:14:39 -0400

    modified authcontroller.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,6 +5,42 @@ const router = express.Router();
 
 router.post('/register', async (req, res) => {
     try {
-        
+        const { username, password } = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
+        await User.create({ username, password: hashedPassword });
+        res.status(201).json({ message: 'User registered successfully.' });
+    } catch (error) {
+        console.error('Error registering user', error);
+        res.status(500).json({ error: 'Internal server error'});
     }
-})
+});
+
+router.post('/login', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const user = await User.findOne({ where: { username } });
+
+        if (!user || !(await bcrypt.compare(password, user.password))) {
+            res.status(401).json({ error: 'Invalid credentials'});
+            return;
+        }
+
+        req.session.user = { id: user.id, username: user.username };
+        res.json({ message: 'Login successful.'});
+    }   catch (error) {
+        console.error('Error logging in:', error);
+        res.status(500).json({ error: 'Internal server error'});
+    }
+});
+
+router.post('/logout', (req, res) => {
+    try {
+        req.session.destroy();
+        res.json({ message: 'Logout successful.'});
+    }   catch (error) {
+        xonosle.error('Error logging out:', error);
+        res.status(500).json({ error: 'Internal server error'});
+    }
+});
+
+module.exports = router;
